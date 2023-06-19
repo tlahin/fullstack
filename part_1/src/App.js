e4e7339a9c8becbe48f9b4815203b147ba50e0d1
tlahin <teemu.lahin@gmail.com>
2023-06-19 16:27:40 +0300

    reorganized the file structure for the course, part_1 done (?)

---
+++ part_1/src/App.js
@@ -0,0 +1,63 @@
+
+const Header = (props) => {
+	return ( 
+		<h1> {props.course} </h1>
+	)
+}
+
+const Part = (props) => {
+	return (
+		<p>
+			{props.part} {props.exercises}
+		</p>
+	)
+}
+
+const Content = (props) => {
+	return (
+		<div>
+			<Part part = {props.parts[0].title} exercises = {props.parts[0].exercises} />
+			<Part part = {props.parts[1].title} exercises = {props.parts[1].exercises} />
+			<Part part = {props.parts[2].title} exercises = {props.parts[2].exercises} />
+		</div>
+	)
+}
+
+const Total = (props) => {
+	return (
+		<p>
+			Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+		</p>
+	)
+}
+
+const App = () => {
+
+	const data = {
+		title: 'Half Stack application development',
+		parts: [
+			{
+				title: 'Fundamentals of React',
+				exercises: 10
+			},
+			{
+				title: 'Using props to pass data',
+				exercises: 7
+			},
+			{
+				title: 'State of a component',
+				exercises: 14
+			}
+		]
+	}
+
+	return (
+		<div>
+			<Header title = {data.title} />
+			<Content parts = {data.parts} />
+			<Total parts = {data.parts} />
+		</div>
+	)
+}
+
+export default App
